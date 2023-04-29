905b680e702ea4b7942ecfbdc082eba77ccd3330
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-04-29 19:48:34 +0700

    nav collapse

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -99,21 +99,31 @@
             </div>
           </header>
 
-          {{-- <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-dark p-4">
-              <h5 class="text-white h4">Collapsed content</h5>
-              <span class="text-body-secondary">Toggleable via the navbar brand.</span>
-            </div>
-          </div>
-          <nav class="navbar navbar-dark bg-dark">
-            <div class="container-fluid">
+
+
+          <nav class="navbar-collapse navbar-dark bg-dark d-md-none d-lg-none d-xl-none d-xxl-none ">
+            <div class="container-fluid d-flex justify-content-end">
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="bi bi-chevron-down dropdown-indicator"></i>
               </button>
             </div>
           </nav>
- --}}
- 
+
+          <div class="collapse d-md-none d-lg-none d-xl-none d-xxl-none" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+                <ul>
+                    <li class="sidebar-fiture text-center"><a class="nav-link" href="blank.html">
+                        <span>Dashboard</span></a></li>
+                      <li class="sidebar-fiture text-center"><a class="nav-link" href="blank.html">
+                        <span>Pemesanan</span></a></li>
+                      <li class="sidebar-fiture text-center"><a class="nav-link" href="blank.html">
+                        <span>Profile</span></a></li>
+                </ul>
+            </div>
+          </div>
+
+        {{--   d-lg-none d-xl-none d-xxl-none --}}
+
       <div class="sidebar d-none d-md-inline-block" >
         <aside id="sidebar-wrapper">
           <ul class="sidebar-menu container">
